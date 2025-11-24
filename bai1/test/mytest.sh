f90e492eef4cc5f622827cb3dd698a3d61759174
Nganha <nganhaa.236@gmail.com>
2025-11-24 15:21:45 +0700

    init

---
+++ bai1/test/mytest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCANNER="../incompleted/scanner"
+
+$SCANNER example1.kpl | diff result1.txt -
+# $SCANNER example2.kpl | diff result2.txt -
+# $SCANNER example3.kpl | diff result3.txt -
+# $SCANNER example4.kpl | diff result4.txt -
+# $SCANNER example5.kpl | diff result5.txt -
+# $SCANNER example6.kpl | diff result6.txt -
